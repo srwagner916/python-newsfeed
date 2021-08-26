92be6684c2847747fc238c1d98cf22ec0e286923
Shay Wagner <srwagner916@gmail.com>
2021-08-26 18:47:20 -0500

    can now see posts on homepage and dates urls and plural words are formatted properly

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from app.routes import home, dashboard
+from app.db import init_db
+from app.utils import filters
 
 def create_app(test_config=None):
   # set up app config
@@ -9,6 +11,10 @@ def create_app(test_config=None):
     SECRET_KEY='super_secret_key'
   )
 
+  app.jinja_env.filters['format_url'] = filters.format_url
+  app.jinja_env.filters['format_date'] = filters.format_date
+  app.jinja_env.filters['format_plural'] = filters.format_plural
+
   @app.route('/hello')
   def hello():
     return 'hello world'
@@ -17,4 +23,12 @@ def create_app(test_config=None):
   app.register_blueprint(home)
   app.register_blueprint(dashboard)
 
+  init_db(app)
+
   return app
+
+def close_db(e=None):
+  db = g.pop('db', None)
+
+  if db is not None:
+    db.close()
