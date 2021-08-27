7f0da71342bedff587eeed651d00ef60f676e24f
Shay Wagner <srwagner916@gmail.com>
2021-08-27 14:39:31 -0500

    implemented a login route

--- app/routes/home.py
+++ app/routes/home.py
@@ -18,7 +18,12 @@ def index():
 
 @bp.route('/login')
 def login():
-  return render_template('login.html')
+  print(session)
+  # not logged in yet
+  if session.get('loggedIn') is None:
+    return render_template('login.html')
+  
+  return redirect('/dashboard')
 
 @bp.route('/post/<id>')
 def single(id):
